45b4c55563658d1632bef1b722d19435d49d094c
asuahmed759 <asuahmed759@gmail.com>
2026-06-13 18:33:05 +0500

    Add SMS Spam Classifier project files

---
+++ main.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+import numpy as np
+
+ps = PorterStemmer()
+
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+st.title("Email/SMS Spam Classifier")
+
+input_sms = st.text_area("Enter the message")
+
+if st.button('Predict'):
+    # 1. preprocess
+    transformed_sms = transform_text(input_sms)
+    # 2. vectorize
+    vector_input = tfidf.transform([transformed_sms]).toarray()
+    # 3. Add extra features (num_chars, num_words, num_sents)
+    num_chars = len(transformed_sms)
+    num_words = len(transformed_sms.split())
+    num_sents = len(transformed_sms.split('.'))
+    extra = np.array([[num_chars, num_words, num_sents]])
+    vector_input = np.hstack((vector_input, extra))
+    # 4. predict
+    result = model.predict(vector_input)[0]
+    # 5. Display
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
